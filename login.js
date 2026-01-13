151e710946485f3d6ab4fcf5ec274a9b84e5a6fd
saleena <saleenagul35@gmail.com>
2026-01-13 13:56:16 +0500

    editing

--- login.js
+++ login.js
@@ -10,3 +10,31 @@ if (localStorage.getItem("theme") === "lighter-mode") {
 
     }); 
 }
+
+document.getElementById("email").oninput=function(){
+let EmailInput = this.value
+sessionStorage.setItem("email",EmailInput);
+}
+document.getElementById("password").oninput=function(){
+let passwordInput = this.value;
+sessionStorage.setItem("password",passwordInput);
+}
+document.getElementById("passwordRepeat").oninput=function(){
+    let passwordRepeatInput = this.value;
+    sessionStorage.setItem("passwordRepeat", passwordRepeatInput)
+}
+
+let signupbtn = document.getElementsByClassName("signupbtn");
+signupbtn[0].onclick = function(){
+if(sessionStorage.getItem("email")&&sessionStorage.getItem("password")&&sessionStorage.getItem("passwordRepeat")){
+
+
+    alert("Congrats you are successfully Logged in")
+}else{
+    alert("kindly fill the form to Log In / Sign Up")
+}
+
+}
+
+
+
